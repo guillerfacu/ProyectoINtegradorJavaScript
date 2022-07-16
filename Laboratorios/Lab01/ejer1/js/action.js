75608dd0eb9c79c1ae26c25bd394ddb1a4fe0c78
guillerfacu <guillerfacu@gmail.com>
2022-07-16 00:17:22 -0300

    Lab01

---
+++ Laboratorios/Lab01/ejer1/js/action.js
@@ -0,0 +1,28 @@
+
+
+let fecha = new Date();
+let diaS = fecha.getDay();
+
+switch(diaS){
+
+    case 0 : alert('River');
+    break;
+    case 1 : alert('Arranca la semana');
+    break;
+    case 2 : alert('Nada');
+    break;
+    case 3 : alert('Copa');
+    break;
+    case 4 : alert('Clase');
+    break;
+    case 5 : alert('Precia');
+    break;
+    case 6 : alert('Joda');
+    break;
+    default : alert('Error....');
+    break;
+
+}
+
+
+
