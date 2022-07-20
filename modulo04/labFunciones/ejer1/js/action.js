06fa3646494746048c6e278ea43dff96eacb12a9
guillerfacu <guillerfacu@gmail.com>
2022-07-20 09:51:02 -0300

    Prueba con Jquery $

--- modulo04/labFunciones/ejer1/js/action.js
+++ modulo04/labFunciones/ejer1/js/action.js
@@ -1,4 +1,4 @@
-
+/* 
 function alertCaja(color){
     alert('El color es: ' +color)
 }
@@ -15,4 +15,19 @@ function colorearCaja(color){
     alert('El color es ' +color);
     document.getElementById('caja').innerHTML = 'Él color es' + color;
     document.getElementById('caja').style.background = color;
-}
+} */
+
+console.log($);
+
+const panel = $('#panel');
+const paneles = $('.paneles');
+const boton = $('button'); 
+
+
+boton.on('click' , function(){
+    alert('Hiciste click en el boton');
+});
+
+boton.click(function(){
+    alert('Hiciste click en el boton');
+});
