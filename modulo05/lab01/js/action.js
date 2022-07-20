1f2334311c55969bbf0590e9b3199d65f7bf9f9d
guillerfacu <guillerfacu@gmail.com>
2022-07-20 08:59:07 -0300

    Modulo05lab01 correjido

--- modulo05/lab01/js/action.js
+++ modulo05/lab01/js/action.js
@@ -1,6 +1,16 @@
-var numeroActual = 0;
+/* var numeroActual = 0;
 do{
     var numeroActual = Number(prompt("Ingrese un numero actual"));
     var suma = suma + numeroActual;
 }while ((continaur = confirm("¿Desea seguir ingresando numeros?")));
-alert("Total ingresado: "+suma);
+alert("Total ingresado: "+suma); */
+
+
+
+let numeroActual = 0;
+let suma = 0;
+do{
+    numeroActual = Number(prompt("Ingrese un numero actual"));
+    suma += numeroActual;
+}while ((continaur = confirm("¿Desea seguir ingresando numeros?")));
+alert(`Total ingresado: ${suma}`);
