63b1aa6a6e7d745e8f746eec985931e586145675
guillerfacu <guillerfacu@gmail.com>
2022-07-20 02:54:52 -0300

    Modulo4[Lab01Ok

--- modulo04/labFunciones/ejer1/js/action.js
+++ modulo04/labFunciones/ejer1/js/action.js
@@ -1,5 +1,5 @@
 
-function colorearCaja(color){
+function alertCaja(color){
     alert('El color es: ' +color)
 }
 
@@ -8,8 +8,11 @@ for(let i = 0; i< boton.length; i++){
     boton[i].onclick = function(){
         colorearCaja(this.innerHTML);
     }
+ 
 }
 
-function crearCaja(){
-    
+function colorearCaja(color){
+    alert('El color es ' +color);
+    document.getElementById('caja').innerHTML = 'Él color es' + color;
+    document.getElementById('caja').style.background = color;
 }
