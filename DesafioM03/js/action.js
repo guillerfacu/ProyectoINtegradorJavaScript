3a96ec5e92337d7b6f6b1e18a71985b4e46a941e
guillerfacu <guillerfacu@gmail.com>
2022-07-19 23:50:15 -0300

    modulo 3 desafios

---
+++ DesafioM03/js/action.js
@@ -0,0 +1,41 @@
+
+
+
+let ofertas = [
+
+    {
+        nombre: 'Plan01',
+        capital: 150000,
+        plazo: 30,
+        tasa: 0.15
+    },
+
+    {
+        nombre: 'Plan02',
+        capital: 300000,
+        plazo: 180,
+        tasa: 0.1
+
+    },
+
+    {
+        nombre: 'Plan03',
+        capital: 485000,
+        plazo: 60,
+        tasa: 0.23
+    }
+
+
+];
+
+let ofertasConIntereses = [];
+
+
+
+
+
+
+
+
+
+
